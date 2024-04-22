9c32557ea715715623c70f68c799a80453b4f73b
Aiyanur <161850438+Aiyanur@users.noreply.github.com>
2024-04-22 11:28:07 +0200

    head.php, footer.php, nav,php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div id="footer-main">
+        <div class="slpce">
+            <p>
+                It contains the work of Aiya (Aiyanur) and talks about the K Pop band known as BTS.
+                The name stands for the Korean phrase Bangtan Sonyeondan (Korean: 방탄소년단; Hanja: 防彈少年團),
+                which translates literally to 'Bulletproof Boy Scouts'.
+            </p>
+        </div>
+        <div class="copyright">
+            <p> © Aiyanur Ramazan</p>
+        </div>
+    </div>
+</footer>
