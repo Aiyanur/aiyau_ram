f639a4525a544b42bbc58a1053b3e656bdfff140
Aiyanur <161850438+Aiyanur@users.noreply.github.com>
2024-04-22 11:18:52 +0200

    vztvorila som link nav.php a vytvorila som nav.php

--- contact.php
+++ contact.php
@@ -3,16 +3,7 @@
 <?php include 'head.php' ?>
 <body class="contact" style="background-color: darkgray;">
     <div class="container">
-        <nav>
-        <a href="#header"><img src="images/BTS_Logo.png" class="logo" /></a>
-        <ul>
-            <li><a href="index.html" style="color: #262626;">Home</a></li>
-            <li><a href="members.html" style="color: #262626;">Fun Facts</a></li>
-            <li><a href="contact.html" style="color: #262626;">Contact</a></li>
-            <li><a href="discography.html" style="color: #262626;">Discography</a></li>
-            <li><a href="thankyou.html" style="color: #262626;">Gratitude</a></li>
-        </ul>
-        </nav>
+        <?php include 'nav.php' ?>
     </div>
 
 <div id="contact">
