fe2b0a98ac96bb9dc48530280b6c618ff9c3066c
Aiyanur <ayyanurramazan@gmail.com>
2024-05-26 11:41:28 +0200

    Pridaný register_form

---
+++ register_form.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<?php include_once "parts/head.php" ?>
+<body>
+<div class="form-container">
+
+    <form action="" method="post">
+        <h3>register now</h3>
+        <input type="email" name="usermail" placeholder="enter your email" class="box" required>
+        <input type="password" name="password" placeholder="enter your password" class="box" required>
+        <input type="password" name="cpassword" placeholder="confirm your password" class="box" required>
+        <input type="submit" value="register now" name="submit" class="form-btn">
+        <p>already have an account? <a href="login_form.php">login now</a></p>
+    </form>
+
+
+</div>
+</body>
+</html>
